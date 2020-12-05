9d4d6153a79c0e024a16894287093e6f4fffef34
muksthonge <muksthonge2020@gmail.com>
2020-12-05 13:02:20 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component, VERSION } from "@angular/core";
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  name = "Angular12222 " + VERSION.major;
+  name = "Angular12222rrttttt " + VERSION.major;
 }
